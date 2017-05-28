ac6f2b1af3487543217178070b29a030775db8bb
d_ojima <daiking.ca2@gmail.com>
2017-05-29 08:44:15 +0900

    session check を行う関数を追加

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -5,14 +5,25 @@ let user = require('./user.js');
 let drip = require('./coffee.js');
 let type = require('./type.js');
 let pasori = require('./pasori.js');
-router.use('/user', user);
-router.use('/drip', drip);
-router.use('/type', type);
-router.use('/pasori', pasori);
+
+// check login session
+let loginCheck = (req, res, next) => {
+  if (req.session.user_id) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', loginCheck, function(req, res, next) {
   res.render('index', { title: 'バリスタ野郎 β' });
 });
+
+router.use('/user', loginCheck, user);
+router.use('/drip', loginCheck, drip);
+router.use('/type', loginCheck, type);
+router.use('/pasori', loginCheck,  pasori);
+
 
 module.exports = router;
